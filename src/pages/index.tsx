849596ee91bdb13d22cea26b26237708e903cbd2
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-07-08 07:33:50 -0400

    Change Home Msg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <section className="w-full h-full flex items-center justify-center text-5xl font-bold text-center">
-      <h1>! Welcome To Products Information App !</h1>
+      <h1>! Welcome To Products App !</h1>
     </section>
   )
 }
